1b39889ce0994352959c04e875822e938f2c403a
eeqi98 <lim.ee@northeastern.edu>
2024-04-10 15:48:04 -0700

    implement quizzes screens

---
+++ src/Kanbas/Courses/Quizzes/Details/index.tsx
@@ -0,0 +1,129 @@
+import { useParams } from "react-router-dom";
+import { FaCircleCheck } from "react-icons/fa6";
+import { FaEllipsisV } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
+import { quizzes } from "../../../Database";
+
+function QuizDetails() {
+    const { quizId } = useParams();
+    const quiz = quizzes.find((quiz) => {
+        return quiz.items.some((item) => item.item_id === quizId);
+    });
+    const quizItem = quiz?.items.find((item) => item.item_id === quizId);
+    const formatDate = (dateString: string | number | Date) => {
+        return new Date(dateString).toLocaleString('en-US', {
+            month: 'short',
+            day: 'numeric',
+        });
+    };
+    const formatTime = (dateString: string | number | Date) => {
+        return new Date(dateString).toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        });
+    };
+    return (
+        <div className="container-fluid me-3 ms-3">
+
+            <div className="row pt-3">
+                <div className="col text-end">
+                    <div className="text-success fs-5 me-1 d-inline">
+                        <button type="button" className="btn btn-success">
+                            <FaCircleCheck className="me-1" /> Published
+                        </button>
+                    </div>
+                    <div className="d-inline">
+                        <a className="btn border bg-light me-1">Perview</a>
+                    </div>
+                    <div className="d-inline">
+                        <a className="btn border bg-light me-1"><FaPencil style={{ transform: "scaleX(-1)" }} /> Edit</a>
+                    </div>
+                    <div className="d-inline">
+                        <a className="btn border bg-light me-1"><FaEllipsisV /></a>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <h1>{quizItem?.item_name}</h1>
+            <br />
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Quiz Type</b></div>
+                <div className="col-sm-5">{quizItem?.item_name}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Points</b></div>
+                <div className="col-sm-5">{quizItem?.points}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Assignment Group</b></div>
+                <div className="col-sm-5">{quizItem?.group}</div></div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Shuffle Answer</b></div>
+                <div className="col-sm-5">{quizItem?.shuffle}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Time Limit</b></div>
+                <div className="col-sm-5">{quizItem?.time_limit}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Multiple Attempts</b></div>
+                <div className="col-sm-5">{quizItem?.multiple_attempts}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>View Responses</b></div>
+                <div className="col-sm-5">{quizItem?.reponses}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Show Correct Answer</b></div>
+                <div className="col-sm-5">{quizItem?.show_ans}</div></div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>One Question at a Time</b></div>
+                <div className="col-sm-5">{quizItem?.one_q_per_time}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Require Respondus LockDown Browser</b></div>
+                <div className="col-sm-5">{quizItem?.lockdown_browser}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Required to View Quiz Results</b></div>
+                <div className="col-sm-5">{quizItem?.view_results}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Webcam Required</b></div>
+                <div className="col-sm-5">{quizItem?.webcam}</div>
+            </div>
+            <div className="mb-3 row">
+                <div className="col-sm-5 text-sm-end mb-2 mb-sm-0"><b>Lock Questions After Answering</b></div>
+                <div className="col-sm-5">{quizItem?.lock_questions}</div>
+            </div>
+            <br />
+            <div className="row">
+                <div className="col-md-3"><b>Due</b></div>
+                <div className="col-md-2"><b>For</b></div>
+                <div className="col-md-4"><b>Available from</b></div>
+                <div className="col-md-3"><b>Until</b></div>
+            </div>
+            <hr />
+            <div className="row">
+                <div className="col-md-3">
+                    {quizItem?.due_date && (
+                    <>{formatDate(quizItem.due_date)} at {formatTime(quizItem.due_date)}</>
+                )}
+                </div>
+                <div className="col-md-2">{quizItem?.assign_to}</div>
+                <div className="col-md-4">{quizItem?.due_date && (
+                    <>{formatDate(quizItem.available_from_date)} at {formatTime(quizItem.available_from_date)}</>
+                )}
+                </div>
+                <div className="col-md-3">{quizItem?.due_date && (
+                    <>{formatDate(quizItem.available_to_date)} at {formatTime(quizItem.available_to_date)}</>
+                )}
+                </div>
+            </div>
+            <hr />
+        </div>
+    );
+}
+
+export default QuizDetails;
---
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -0,0 +1,128 @@
+import { FaCheckCircle, FaEllipsisV, FaPlus, FaSortDown } from "react-icons/fa";
+import { HiOutlinePencilSquare } from "react-icons/hi2";
+import { PiProhibit } from "react-icons/pi";
+import { Link, useParams } from "react-router-dom";
+import "./index.css";
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { quizzes } from "../../Database";
+
+function DeleteQuizModal({ show, onClose, onDelete }: { show: boolean, onClose: () => void, onDelete: () => void }) {
+    return (
+        <Modal show={show} onHide={onClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Confirm Deletion</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                Are you sure you want to delete this quiz?
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => { onClose() }}>Cancel</Button>
+                <Button variant="danger" onClick={() => { onDelete() }}>Delete</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+function Quizzes() {
+    const { courseId } = useParams();
+    const quizList = quizzes?.filter((quiz: any) => quiz.course === courseId) ?? [];
+    const formatDate = (dateString: string | number | Date) => {
+        return new Date(dateString).toLocaleString('en-US', {
+            month: 'short',
+            day: 'numeric',
+        });
+    };
+    const formatTime = (dateString: string | number | Date) => {
+        return new Date(dateString).toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        });
+    };
+    const [showDeleteQuizModal, setShowDeleteQuizModal] = useState(false);
+    return (
+        <div className="flex-fill me-2 ms-2 mt-2">
+            <div className="d-flex flex-row justify-content-between ps-3 pe-3">
+                <div className="w-25">
+                    <input className="form-control" type="text" placeholder="Search for Quizzes" title="Search by Quiz name"></input>
+                </div>
+                <div>
+                    <div className="d-inline">
+                        <Link to={`/Kanbas/Courses/${courseId}/Quizzes/Add`} className="btn ms-1 red-button border border-dark" ><FaPlus />Quiz</Link>
+                    </div>
+                    <a className="btn ms-1 ps-1 pe-1 border border-dark bg-light"><FaEllipsisV /></a>
+
+                </div>
+            </div>
+            <hr />
+            <ul className="list-group wd-courses-quizzes">
+                {quizList.map((quiz) => (
+                    <li key={quiz._id} className="list-group-item">
+                        <div>
+                            <FaEllipsisV className="me-3 ms-2" />
+                            <a className="btn me-3 " data-bs-toggle="collapse" href={`#collapse-${quiz.category}-list`} role="button" aria-expanded="false" aria-controls={`collapse-${quiz.category}-list`}>
+                                <FaSortDown style={{ verticalAlign: "top" }} />
+                            </a>
+                            <span className="fw-bold">{quiz.category}</span>
+                        </div>
+                        <div className="collapse show p-0" id={`collapse-${quiz.category}-list`}>
+                            <ul className="listGroup">
+                                {quiz.items?.map((item: any) => (
+                                    <li key={item.item_id} className="list-group-item">
+                                        <div className="d-flex align-items-center">
+                                            <FaEllipsisV className="me-2 ms-2" />
+                                            <HiOutlinePencilSquare className="me-3 ms-2 text-success" />
+                                            <div className="flex-fill">
+                                                <div>
+                                                    <Link className="fw-bold quiz-list-link text-dark" to={`/Kanbas/Courses/${courseId}/Quizzes/${quiz._id}/${item.item_id}`}>
+                                                        {item.item_name}
+                                                    </Link>
+                                                </div>
+                                                <div className="wd-courses-quizzes-minor-text">
+                                                    {item.available_from_date && new Date(item.available_from_date) > new Date() ? (
+                                                        <span><b>Not available until </b>
+                                                            {formatDate(item.available_from_date)} at {formatTime(item.available_from_date)} | </span>
+                                                    ) : (
+                                                        item.available_to_date && new Date(item.available_to_date) <= new Date() && (
+                                                            <span><b>Closed</b> | </span>
+                                                        ))}
+                                                    <b>Due</b> {formatDate(item.due_date)} at {formatTime(item.due_date)} | {item.points} pts | {item.question_count} Questions
+                                                </div>
+                                            </div>
+                                            <span className="ms-auto">
+                                                {item.available === "No" && item.published === "No" ? (
+                                                    <PiProhibit />
+                                                ) : (
+                                                    < FaCheckCircle className="text-success" />
+                                                )}
+                                                <div className="dropleft d-inline">
+                                                    <a className="btn wd-courses-quizzes-icon-link" type="button" data-bs-toggle="dropdown" aria-expanded="false"><FaEllipsisV /></a>
+                                                    <ul className="dropdown-menu">
+                                                        <li><button className="dropdown-item">Delete</button></li>
+                                                        <li><button className="dropdown-item">
+                                                            <Link style={{ "textDecoration": "None", "color": "black" }}
+                                                                to={`/Kanbas/Courses/${courseId}/Quizzes/${quiz._id}/${item.item_id}`}>
+                                                                Edit
+                                                            </Link>
+                                                        </button></li>
+                                                        <li><button className="dropdown-item">Publish</button></li>
+                                                    </ul>
+                                                </div>
+                                            </span>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+            <DeleteQuizModal
+                show={showDeleteQuizModal}
+                onClose={() => setShowDeleteQuizModal(false)}
+                onDelete={() => { }} />
+        </div>
+    )
+}
+
+export default Quizzes;
