2e0fc9949f49ee8c423a5463c42eb51c409c864b
KaijunChen0 <113864900+KaijunChen0@users.noreply.github.com>
2024-04-15 20:35:03 -0700

    add fillinblank and true/false

--- src/Kanbas/Courses/Quizzes/EditQuestions/EditFillInBlanksQuestion.tsx
+++ src/Kanbas/Courses/Quizzes/EditQuestions/EditFillInBlanksQuestion.tsx
@@ -1,61 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import '../index.css';
-import { Dropdown } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "../index.css";
+import { Dropdown } from "react-bootstrap";
 // import { Answer, QuestionEditorState } from './index';
-import { FaPlus, FaTrash } from 'react-icons/fa';
+import { FaPlus, FaTrash } from "react-icons/fa";
 
 export interface FillInBlanksQuestionType {
-    questionId: string;
-    title: string;
-    points: number;
-    questionText: string;
-    correctAnswer: string;
-    questionType: string;//M for multiple choice, T for true/false, B for fill in the blank
-    quizId: string;
+  questionId: string;
+  title: string;
+  points: number;
+  questionText: string;
+  correctAnswer: string;
+  questionType: string; //M for multiple choice, T for true/false, B for fill in the blank
+  quizId: string;
 }
 
 function EditFillInBlanksQuestion() {
-    const initialState: FillInBlanksQuestionType = {
-        questionId: 'question111',
-        title: '',
-        points: 1,
-        questionText: '',
-        correctAnswer: '',
-        questionType: 'B',
-        quizId: 'quiz1',
-      };
-    const [question, setQuestion] = useState<FillInBlanksQuestionType>(initialState);
-    
-      // Function to handle change in the correct answer
-    const handleCorrectAnswerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuestion(prevState => ({
-            ...prevState,
-            correctAnswer: e.target.value
-        }));
-    };
+  const initialState: FillInBlanksQuestionType = {
+    questionId: "question111",
+    title: "",
+    points: 1,
+    questionText: "",
+    correctAnswer: "",
+    questionType: "B",
+    quizId: "quiz1",
+  };
+  const [question, setQuestion] =
+    useState<FillInBlanksQuestionType>(initialState);
 
-    const handleUpdate = () => {
-        // API call to save the current state
-        console.log('Saving data...', question);
-        // Example: axios.post('/api/update-question', question)
-        // .then(response => console.log('Saved successfully'))
-        // .catch(error => console.error('Error saving:', error));
-    }
+  // Function to handle change in the correct answer
+  const handleCorrectAnswerChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setQuestion((prevState) => ({
+      ...prevState,
+      correctAnswer: e.target.value,
+    }));
+  };
 
-    const handleCancel = () => {
-        setQuestion(initialState);
-    }
+  const handleUpdate = () => {
+    // API call to save the current state
+    console.log("Saving data...", question);
+    // Example: axios.post('/api/update-question', question)
+    // .then(response => console.log('Saved successfully'))
+    // .catch(error => console.error('Error saving:', error));
+  };
 
-    // Fetch and set initial data
-    useEffect(() => {
-        // Fetch data from API and set it
-        // setQuestion(fetchedData);
-    }, []);
+  const handleCancel = () => {
+    setQuestion(initialState);
+  };
 
-    return (
-        <div className="container-fluid me-3 ms-3">
+  // Fetch and set initial data
+  useEffect(() => {
+    // Fetch data from API and set it
+    // setQuestion(fetchedData);
+  }, []);
+
+  return (
+    <div className="container-fluid me-3 ms-3">
       {/* Row for Title and Points */}
       <div className="row pt-3">
         <div className="col-4">
@@ -63,21 +66,25 @@ function EditFillInBlanksQuestion() {
             className="form-control"
             type="text"
             value={question.title}
-            onChange={e => setQuestion({ ...question, title: e.target.value })}
+            onChange={(e) =>
+              setQuestion({ ...question, title: e.target.value })
+            }
             placeholder="Question Title"
           />
         </div>
         <div className="col-4">
-        <Dropdown>
-            <Dropdown.Toggle variant="light"  id="dropdown-basic">
-                Fill in the Blank
+          <Dropdown>
+            <Dropdown.Toggle variant="light" id="dropdown-basic">
+              Fill in the Blank
             </Dropdown.Toggle>
             <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Multiple choice</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">True/False</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Fill in the Blank </Dropdown.Item>
+              <Dropdown.Item href="#/action-1">Multiple choice</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">True/False</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Fill in the Blank{" "}
+              </Dropdown.Item>
             </Dropdown.Menu>
-            </Dropdown>
+          </Dropdown>
         </div>
 
         <div className="col-3 d-inline-flex align-items-center">
@@ -86,14 +93,19 @@ function EditFillInBlanksQuestion() {
             className="form-control"
             type="number"
             value={question.points}
-            onChange={e => setQuestion({ ...question, points: parseInt(e.target.value) })}
+            onChange={(e) =>
+              setQuestion({ ...question, points: parseInt(e.target.value) })
+            }
             placeholder="Points"
             style={{ width: "auto" }}
-          />   
-         </div>
+          />
+        </div>
       </div>
-      <hr/>
-      <h5 className="text-muted-question">Enter your question and multiple answers, then select the one correct answer</h5>
+      <hr />
+      <h5 className="text-muted-question">
+        Enter your question and multiple answers, then select the one correct
+        answer
+      </h5>
       <b>Question:</b>
       <div className="row pt-3">
         <div className="col">
@@ -101,34 +113,31 @@ function EditFillInBlanksQuestion() {
             className="custom-quill form-control"
             id="myCustomQuillEditorQuestion"
             value={question.questionText}
-            onChange={value => setQuestion({ ...question, questionText: value })}
+            onChange={(value) =>
+              setQuestion({ ...question, questionText: value })
+            }
             placeholder="Enter your question text here..."
           />
         </div>
       </div>
 
-     
       <div className="row pt-3">
-      <strong>Answers:</strong>
+        <strong>Answers:</strong>
         <div className="input-group mb-2">
-            Correct Answer:
-            <input
+          Correct Answer:
+          <input
             type="text"
             className="form-control"
             value={question.correctAnswer}
             onChange={handleCorrectAnswerChange}
             placeholder={question.correctAnswer}
-            />
+          />
         </div>
       </div>
 
       <div className="row pt-3">
         <div className="col">
-          <button
-            className="btn me-2"
-            type="button"
-            onClick={handleCancel}
-          >
+          <button className="btn me-2" type="button" onClick={handleCancel}>
             Cancel
           </button>
           <button
@@ -141,6 +150,6 @@ function EditFillInBlanksQuestion() {
         </div>
       </div>
     </div>
-      );
+  );
 }
 export default EditFillInBlanksQuestion;
--- src/Kanbas/Courses/Quizzes/EditQuestions/EditTrueFalseQuestion.tsx
+++ src/Kanbas/Courses/Quizzes/EditQuestions/EditTrueFalseQuestion.tsx
@@ -1,55 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import '../index.css';
-import { Dropdown } from 'react-bootstrap';
-import { Answer, QuestionEditorState } from './index';
-import { FaPlus, FaTrash } from 'react-icons/fa';
+import React, { useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "../index.css";
+import { Dropdown } from "react-bootstrap";
+import { Answer, QuestionEditorState } from "./index";
+import { FaPlus, FaTrash } from "react-icons/fa";
 
 function EditTrueFalseQuestion() {
-    const initialState: QuestionEditorState = {
-        questionId: 'question111',
-        title: '',
-        points: 1,
-        questionText: '',
-        correctAnswer: '',//"true" or "false"
-        questionType: 'T',
-        possibleAnswers: [{ answerId: 'a1', text: 'true', questionId: "question111" }, { answerId: 'a2', text: 'false', questionId:"question111" }],
-        quizId: 'quiz1',
-    };
+  const initialState: QuestionEditorState = {
+    questionId: "question111",
+    title: "",
+    points: 1,
+    questionText: "",
+    correctAnswer: "", //"true" or "false"
+    questionType: "T",
+    possibleAnswers: [
+      { answerId: "a1", text: "true", questionId: "question111" },
+      { answerId: "a2", text: "false", questionId: "question111" },
+    ],
+    quizId: "quiz1",
+  };
 
-    const [question, setQuestion] = useState<QuestionEditorState>(initialState);
+  const [question, setQuestion] = useState<QuestionEditorState>(initialState);
 
-      const handleAnswerChange = (text:any, index:any) => {
-        const newAnswers = [...question.possibleAnswers];
-        newAnswers[index].text = text;
-        setQuestion({ ...question, possibleAnswers: newAnswers });
-      };
-    
-      const handleSelectCorrectAnswer = (id:any) => {
-        setQuestion({ ...question, correctAnswer: id });
-      };
+  const handleAnswerChange = (text: any, index: any) => {
+    const newAnswers = [...question.possibleAnswers];
+    newAnswers[index].text = text;
+    setQuestion({ ...question, possibleAnswers: newAnswers });
+  };
 
-      const handleUpdate = () => {
-        // API call to save the current state
-        console.log('Saving data...', question);
-        // Example: axios.post('/api/update-question', question)
-        // .then(response => console.log('Saved successfully'))
-        // .catch(error => console.error('Error saving:', error));
-    }
+  const handleSelectCorrectAnswer = (id: any) => {
+    setQuestion({ ...question, correctAnswer: id });
+  };
 
-    const handleCancel = () => {
-        setQuestion(initialState);
-    }
+  const handleUpdate = () => {
+    // API call to save the current state
+    console.log("Saving data...", question);
+    // Example: axios.post('/api/update-question', question)
+    // .then(response => console.log('Saved successfully'))
+    // .catch(error => console.error('Error saving:', error));
+  };
 
-    // Fetch and set initial data
-    useEffect(() => {
-        // Fetch data from API and set it
-        // setQuestion(fetchedData);
-    }, []);
-    
-      return (
-        <div className="container-fluid me-3 ms-3">
+  const handleCancel = () => {
+    setQuestion(initialState);
+  };
+
+  // Fetch and set initial data
+  useEffect(() => {
+    // Fetch data from API and set it
+    // setQuestion(fetchedData);
+  }, []);
+
+  return (
+    <div className="container-fluid me-3 ms-3">
       {/* Row for Title and Points */}
       <div className="row pt-3">
         <div className="col-4">
@@ -57,21 +60,25 @@ function EditTrueFalseQuestion() {
             className="form-control"
             type="text"
             value={question.title}
-            onChange={(e) => setQuestion({ ...question, title: e.target.value })}
+            onChange={(e) =>
+              setQuestion({ ...question, title: e.target.value })
+            }
             placeholder="Question Title"
           />
         </div>
         <div className="col-4">
-        <Dropdown>
-            <Dropdown.Toggle variant="light"  id="dropdown-basic">
-             True/False
+          <Dropdown>
+            <Dropdown.Toggle variant="light" id="dropdown-basic">
+              True/False
             </Dropdown.Toggle>
             <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Multiple choice</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">True/False</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Fill in the Blank </Dropdown.Item>
+              <Dropdown.Item href="#/action-1">Multiple choice</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">True/False</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Fill in the Blank{" "}
+              </Dropdown.Item>
             </Dropdown.Menu>
-            </Dropdown>
+          </Dropdown>
         </div>
 
         <div className="col-3 d-inline-flex align-items-center">
@@ -80,14 +87,19 @@ function EditTrueFalseQuestion() {
             className="form-control"
             type="number"
             value={question.points}
-            onChange={(e) => setQuestion({ ...question, points: parseInt(e.target.value) })}
+            onChange={(e) =>
+              setQuestion({ ...question, points: parseInt(e.target.value) })
+            }
             placeholder="Points"
             style={{ width: "auto" }}
-          />   
-         </div>
+          />
+        </div>
       </div>
-      <hr/>
-      <h5 className="text-muted-question">Enter your question and multiple answers, then select the one correct answer</h5>
+      <hr />
+      <h5 className="text-muted-question">
+        Enter your question and multiple answers, then select the one correct
+        answer
+      </h5>
       <b>Question:</b>
       <div className="row pt-3">
         <div className="col">
@@ -95,56 +107,50 @@ function EditTrueFalseQuestion() {
             className="custom-quill form-control"
             id="myCustomQuillEditorQuestion"
             value={question.questionText}
-            onChange={(newQuestionText) => setQuestion({ ...question, questionText: newQuestionText})}
+            onChange={(newQuestionText) =>
+              setQuestion({ ...question, questionText: newQuestionText })
+            }
             placeholder="Enter your question text here..."
           />
         </div>
       </div>
-     
+
       <div className="row pt-3">
-      <strong>Answers:</strong>
-      <div className="col-6 input-group mb-3">
-          <span >
-          Correct Answer:
-          </span>
-            <input
-                className="form-control mb-3"
-                type="text"
-                value={question.correctAnswer}
-                placeholder="Click to select correct answer"
-            />
-            </div>
-        {question.possibleAnswers.map((answer, index) => (
-        <div key={answer.answerId} className="input-group mb-3">
-          <span >
-          Possible Answer:
-          </span>
-          <div className="input-group-text">
-            <input
-              type="radio"
-              name="correctAnswer"
-              checked={question.correctAnswer === answer.text}
-              onChange={() => handleSelectCorrectAnswer(answer.text)}
-            />
-          </div>
+        <strong>Answers:</strong>
+        <div className="col-6 input-group mb-3">
+          <span>Correct Answer:</span>
           <input
-            className="form-control"
+            className="form-control mb-3"
             type="text"
-            value={answer.text}
-            onChange={(e) => handleAnswerChange(e.target.value, index)}
-            placeholder={`Possible Answer ${index + 1}`}
+            value={question.correctAnswer}
+            placeholder="Click to select correct answer"
           />
         </div>
-      ))}        
+        {question.possibleAnswers.map((answer, index) => (
+          <div key={answer.answerId} className="input-group mb-3">
+            <span>Possible Answer:</span>
+            <div className="input-group-text">
+              <input
+                type="radio"
+                name="correctAnswer"
+                checked={question.correctAnswer === answer.text}
+                onChange={() => handleSelectCorrectAnswer(answer.text)}
+              />
+            </div>
+            <input
+              className="form-control"
+              type="text"
+              value={answer.text}
+              onChange={(e) => handleAnswerChange(e.target.value, index)}
+              placeholder={`Possible Answer ${index + 1}`}
+            />
+          </div>
+        ))}
       </div>
 
       <div className="row pt-3">
         <div className="col">
-          <button
-            className="btn me-2"
-            type="button"
-            onClick={handleCancel}
-          >
+          <button className="btn me-2" type="button" onClick={handleCancel}>
             Cancel
           </button>
           <button
@@ -157,6 +163,6 @@ function EditTrueFalseQuestion() {
         </div>
       </div>
     </div>
-      );
+  );
 }
 export default EditTrueFalseQuestion;
